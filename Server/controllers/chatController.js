c5ea86fff41db322448042aa091b637b33f52725
Baidik Mazumdar <baidikmazumdar789@gmail.com>
2025-09-24 15:32:49 +0530

    Updated changes

---
+++ Server/controllers/chatController.js
@@ -0,0 +1,20 @@
+import fetch from "node-fetch";
+
+export const handleChat = async (req, res) => {
+  const { message } = req.body;
+  if (!message) return res.json({ response: "Please type a message." });
+
+  try {
+    const response = await fetch("http://localhost:5001/chat", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ message }),
+    });
+
+    const data = await response.json();
+    res.json({ response: data.response });
+  } catch (err) {
+    console.error("Error in chatController:", err);
+    res.json({ response: "Sorry, something went wrong." });
+  }
+};
